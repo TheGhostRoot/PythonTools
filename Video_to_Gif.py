0184f5ec36d7a3a44ebb55af8a118e6469e63953
TheGoldenMine <86381367+TheGhostRoot@users.noreply.github.com>
2024-10-26 13:27:29 +0300

    Delete Video_to_Gif.py

--- Video_to_Gif.py
+++
@@ -1,9 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-while True:
-	name = input("Enter the name of the video >> ")
-	GIFname = input(" Enter the name of the gif >> ")
-	FPSS = int(input("  Enter the fps you want the gif to be >> "))
-	if name != "" or name != " ":
-		clip = VideoFileClip(name)
-		clip.write_gif(GIFname, fps=FPSS)
